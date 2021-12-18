66c022cf6717850b0f1cc58cc370b655bf10fac9
SwapnilH09 <hansrajswapnil@outlook.com>
2021-12-18 20:15:30 +0530

    test deployment using curl

--- virtualHosts/check_deploy.sh
+++ virtualHosts/check_deploy.sh
@@ -7,3 +7,6 @@ curl 192.168.233.130:8080
 echo "..." 
 echo "applying curl hostname -I | awk '{print $1}'"
 curl hostname -I | awk '{print $1}'
+
+############ resources ##############
+## https://www.freecodecamp.org/news/how-to-start-using-curl-and-why-a-hands-on-introduction-ea1c913caaaa/
