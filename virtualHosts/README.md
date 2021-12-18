0eba3f62f28af8e1de3ffed9dba99960c182c2bf
SwapnilH09 <hansrajswapnil@outlook.com>
2021-12-18 19:15:30 +0530

    deployed a website over virtual host

---
+++ virtualHosts/README.md
@@ -0,0 +1,4 @@
+# this directory creates virtual hosts.....
+running virtual_host.sh assumes the prior creation of vhost.conf file
+vhost.conf is located at /etc/apache2/sites-available/vhost.conf and
+provides the DocumentRoot to this website
---
+++ virtualHosts/virtual_host.sh
@@ -0,0 +1,21 @@
+echo "creating document root destination to house our website...."
+sudo mkdir -p /var/www/html/vhost
+
+echo "granting proper ownership to the directory housing website..."
+sudo chown -R $USER:$USER /var/www/html/vhost
+
+echo "granting proper permissions...."
+sudo chmod -R 755 /var/www/html/vhost
+
+echo "write contents for the website...."
+sudo cp /var/www/html/index.html /var/www/html/vhost
+
+echo "creating a virtual host configuration...."
+# change /etc/apache2/sites-available/vhost.conf
+
+echo "create a link from /etc/apache2/sites-available to /etc/apache2/sites-enabled dir..."
+sudo a2ensite vhost.conf
+sudo systemctl reload apache2
+
+echo "server status...."
+sudo systemctl status apache2.service
