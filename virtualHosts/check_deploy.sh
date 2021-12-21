45e1489ad757aa71b1b3cb86a98f46023cf6804d
SwapnilH09 <hansrajswapnil@outlook.com>
2021-12-21 12:31:58 +0530

    curl works with hard-coded url and port num

--- virtualHosts/check_deploy.sh
+++ virtualHosts/check_deploy.sh
@@ -4,9 +4,9 @@
 #echo $ip
 curl 192.168.233.130:8080
 
-echo "..." 
-echo "applying curl hostname -I | awk '{print $1}'"
-curl "`hostname -I`:8080" | awk '{print $1}'
+#echo "...end..." 
+#echo "applying curl hostname -I | awk '{print $1}'"
+#curl "`hostname -I`:8080" | awk '{print $1}'
 
 ############ resources ##############
 ## https://www.freecodecamp.org/news/how-to-start-using-curl-and-why-a-hands-on-introduction-ea1c913caaaa/
--- virtualHosts/curl_comm.sh
+++ virtualHosts/curl_comm.sh
@@ -1,2 +1,11 @@
-var="`hostname -I`"
+var=`hostname -I`
+myip=$var:8080
+myip1="$var:8080"
+
 echo $var
+echo $myip
+echo $myip1
+#echo "$var:abc"
+
+############# resources ##############
+## https://kb.vmware.com/s/article/2097039
