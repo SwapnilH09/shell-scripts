a340048f1d231e287c8651fdfc6fb36f87978078
SwapnilH09 <hansrajswapnil@outlook.com>
2021-12-22 10:10:56 +0530

    improved curl command to include url and port number without hard coding

--- virtualHosts/curl_comm.sh
+++ virtualHosts/curl_comm.sh
@@ -1,11 +1,24 @@
-var=`hostname -I`
-myip=$var:8080
-myip1="$var:8080"
+##var=`hostname -I`
+#myip=$var:8080
+#myip1="$var:8080"
 
-echo $var
-echo $myip
-echo $myip1
+#echo $var
+#echo $myip
+#echo $myip1
 #echo "$var:abc"
 
-############# resources ##############
+#######################################################
+######################################################
+
+var=`hostname -I`
+new_var=`echo $var | sed -e 's/^[[:space:]]*//'`
+myip="$new_var:8080"
+echo "localhost address: $myip"
+
+echo "testing deployment...."
+curl -v $myip
+
+######################################################
+################ resources ####################
 ## https://kb.vmware.com/s/article/2097039
+## https://linuxhint.com/trim_string_bash/
